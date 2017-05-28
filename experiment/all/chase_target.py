8cdb453d944e318ccf3522a32f82cf42a7dc1245
Howard Liu <howardl2@uci.edu>
2017-05-28 03:48:04 -0700

    Add simulation visualization and jump feature

--- experiment/all/chase_target.py
+++ experiment/all/chase_target.py
@@ -27,10 +27,9 @@ import time
 import json
 import errno
 import math
-
 import command
 import simple_parser
-
+import Tkinter as tk
 from collections import namedtuple
 EntityInfo = namedtuple('EntityInfo', 'x, y, z, yaw, pitch, name')
 # EntityInfo = namedtuple('EntityInfo', 'x, y, z, yaw, pitch, name, colour, variation, quantity')
@@ -41,6 +40,7 @@ AgentInfo.__new__.__defaults__ = (0, 0, 0, 0, 0, "")
 
 
 # Task params:
+AGENT_TYPE = "Agent Host"
 NUM_GOALS = 1
 GOAL_TYPE = "Pig"
 GOAL_REWARD = 100
@@ -58,51 +58,87 @@ agent_mob_weight = -10
 agent_turn_weight = 0 # Negative values to penalise turning, positive to encourage.
 
 
+#Our display parameters
+CANVAS_BORDER = 20
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = CANVAS_BORDER + ((CANVAS_WIDTH - CANVAS_BORDER) * ARENA_BREADTH / ARENA_WIDTH)
+CANVAS_SCALEX = (CANVAS_WIDTH-CANVAS_BORDER)/ARENA_WIDTH
+CANVAS_SCALEY = (CANVAS_HEIGHT-CANVAS_BORDER)/ARENA_BREADTH
+CANVAS_ORGX = -ARENA_WIDTH/CANVAS_SCALEX
+CANVAS_ORGY = -ARENA_BREADTH/CANVAS_SCALEY
+
+
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 
-spawn_end_tag = ' type="mob_spawner" variant="' + MOB_TYPE + '"/>'
-missionXML = '''<?xml version="1.0" encoding="UTF-8" ?>
-    <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-        <About>
-            <Summary>Normal life</Summary>
-        </About>
+def getCorner(index,top,left,expand=0,y=206):
+    ''' Return part of the XML string that defines the requested corner'''
+    x = str(-(expand+ARENA_WIDTH/2)) if left else str(expand+ARENA_WIDTH/2)
+    z = str(-(expand+ARENA_BREADTH/2)) if top else str(expand+ARENA_BREADTH/2)
+    return 'x'+index+'="'+x+'" y'+index+'="' +str(y)+'" z'+index+'="'+z+'"'
 
-        <ServerSection>
-            <ServerHandlers>
-                <FlatWorldGenerator generatorString="3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"/>
-                    <DrawingDecorator>
-                        <DrawEntity x="0" y="56" z="10" type="Pig"/>
-                        <DrawEntity x="10" y="56" z="10" type="Sheep"/>
-                        <DrawEntity x="10" y="56" z="0" type="Cow"/>
-                        <!--<DrawBlock type="mob_spawner" variant="Pig" x="0" y="56" z="0"/>-->
-                    </DrawingDecorator>
-            </ServerHandlers>
-        </ServerSection>
+############GENERATE THE XML#################
+def getMissionXML():
+    spawn_end_tag = ' type="mob_spawner" variant="' + MOB_TYPE + '"/>'
+    missionXML = '''<?xml version="1.0" encoding="UTF-8" ?>
+        <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <About>
+                <Summary>Normal life</Summary>
+            </About>
 
-        <AgentSection mode="Survival">
-            <Name>Rover</Name>
-            <AgentStart>
-                <Placement x="0.5" y="56.0" z="0.5" yaw="0"/>
-                <Inventory>
-                    <InventoryBlock slot="0" type="glowstone" quantity="63"/>
-                </Inventory>
-            </AgentStart>
-            <AgentHandlers>
-                <AbsoluteMovementCommands />
-                <DiscreteMovementCommands />
-                <ContinuousMovementCommands turnSpeedDegs="360"/>
-                <InventoryCommands />
-                <ObservationFromFullStats/>
-                <ObservationFromNearbyEntities>
-                    <Range name="entities" xrange="'''+str(ARENA_WIDTH)+'''" yrange="2" zrange="'''+str(ARENA_BREADTH)+'''" />
-                </ObservationFromNearbyEntities>
-                <ObservationFromDiscreteCell/>
-            </AgentHandlers>
-        </AgentSection>
+            <ServerSection>
+                <ServerInitialConditions>
+                    <Time>
+                        <StartTime>6000</StartTime>
+                    </Time>
+                </ServerInitialConditions>
+                <ServerHandlers>
+                    <FlatWorldGenerator generatorString="3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"/>
+                        <DrawingDecorator>
+                            <DrawCuboid ''' + getCorner("1",True,True,expand=1) + " " + getCorner("2",False,False,y=226,expand=1) + ''' type="sandstone"/>
+                            <DrawCuboid ''' + getCorner("1",True,True,y=207) + " " + getCorner("2",False,False,y=226) + ''' type="air"/>
+                            <DrawEntity x="0" y="210" z="10" type="Pig"/>
+                            <DrawBlock x="0" y="211" z="8" type="sand"/>
+                            <DrawBlock x="1" y="211" z="8" type="sand"/>
+                            <DrawBlock x="2" y="211" z="8" type="sand"/>
+                            <DrawBlock x="3" y="211" z="8" type="sand"/>
+                            <DrawBlock x="4" y="211" z="8" type="sand"/>
+                            <DrawBlock x="-1" y="211" z="8" type="sand"/>
+                            <DrawBlock x="-2" y="211" z="8" type="sand"/>
+                            <DrawBlock x="-3" y="211" z="8" type="sand"/>
+                            <DrawEntity x="10" y="210" z="10" type="Sheep"/>
+                            <DrawEntity x="10" y="210" z="0" type="Cow"/>
+                            <!--<DrawBlock type="mob_spawner" variant="Pig" x="0" y="56" z="0"/>-->
+                        </DrawingDecorator>
+                </ServerHandlers>
+            </ServerSection>
 
-    </Mission>'''
+            <AgentSection mode="Survival">
+                <Name>Rover</Name>
+                <AgentStart>
+                    <Placement x="0.5" y="210" z="0.5" yaw="0"/>
+                    <Inventory>
+                        <InventoryBlock slot="0" type="glowstone" quantity="63"/>
+                    </Inventory>
+                </AgentStart>
+                <AgentHandlers>
+                    <AbsoluteMovementCommands />
+                    <DiscreteMovementCommands />
+                    <ContinuousMovementCommands turnSpeedDegs="360"/>
+                    <InventoryCommands />
+                    <ObservationFromFullStats/>
+                    <ObservationFromRecentCommands/>
+                    <ObservationFromNearbyEntities>
+                        <Range name="entities" xrange="'''+str(ARENA_WIDTH)+'''" yrange="25" zrange="'''+str(ARENA_BREADTH)+'''" />
+                    </ObservationFromNearbyEntities>
+                    <ObservationFromDiscreteCell/>
+                </AgentHandlers>
+            </AgentSection>
+
+        </Mission>'''
+    return missionXML
 
 
+###########Finding object functions##########
 def getClosestEntity(agent, entities):
     ag_x = agent[0]
     ag_z = agent[2]
@@ -180,8 +216,45 @@ def getDistanceToGoal(entities):
     dist = abs(ag_x-ent[0])+abs(ag_z-ent[2])
     return dist
 
+
+#########CANVAS FUNCTIONS###################
+def canvasX(x):
+    return (CANVAS_BORDER/2) + (0.5 + x/float(ARENA_WIDTH)) * (CANVAS_WIDTH-CANVAS_BORDER)
+
+def canvasY(y):
+    return (CANVAS_BORDER/2) + (0.5 + y/float(ARENA_BREADTH)) * (CANVAS_HEIGHT-CANVAS_BORDER)
+
+def drawMobs(entities):
+    canvas.delete("all")
+    canvas.create_rectangle(canvasX(-ARENA_WIDTH/2), canvasY(-ARENA_BREADTH/2), canvasX(ARENA_WIDTH/2), canvasY(ARENA_BREADTH/2), fill="#888888")
+    for ent in entities:
+        if ent.name == MOB_TYPE:
+            canvas.create_oval(canvasX(ent.x)-2, canvasY(ent.z)-2, canvasX(ent.x)+2, canvasY(ent.z)+2, fill="#4422ff")
+        elif ent.name == GOAL_TYPE:
+            canvas.create_oval(canvasX(ent.x)-3, canvasY(ent.z)-3, canvasX(ent.x)+3, canvasY(ent.z)+3, fill="#4422ff")
+        elif ent.name == AGENT_TYPE:
+            canvas.create_oval(canvasX(ent.x)-4, canvasY(ent.z)-4, canvasX(ent.x)+4, canvasY(ent.z)+4, fill="#22ff44")
+        else:
+            canvas.create_oval(canvasX(ent.x)-4, canvasY(ent.z)-4, canvasX(ent.x)+4, canvasY(ent.z)+4, fill="#ff00ff")
+    root.update()
+
+def parseCommands(commandsList):
+    for command in commandsList:
+        if "move 1" == command:
+            return True
+    return False
+
 # Create default Malmo objects:
 # <ContinuousMovementCommands />
+
+
+#SETUP the canvas environment
+root = tk.Tk()
+root.wm_title("NLP Simulation View")
+
+canvas = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, borderwidth=0, highlightthickness=0, bg="black")
+canvas.pack()
+root.update()
 
 agent_host = MalmoPython.AgentHost()
 try:
@@ -194,7 +267,7 @@ if agent_host.receivedArgument("help"):
     print agent_host.getUsage()
     exit(0)
 
-my_mission = MalmoPython.MissionSpec(missionXML, True)
+my_mission = MalmoPython.MissionSpec(getMissionXML(), True)
 # my_mission_record = MalmoPython.MissionRecordSpec()
 my_mission_record = MalmoPython.MissionRecordSpec( "./chase_pig.tgz")
 my_mission_record.recordCommands()
@@ -230,18 +303,46 @@ print "Mission running ",
 
 best_yaw = 0
 current_yaw = 0
+count = 0
+posCounter = 0
+trackX = 0
+trackY = 0
+trackZ = 0
+currentX = 0
+currentY = 0
+currentZ = 0
 
+HOW_LONG_TO_WAIT = 2
 # Loop until mission ends:
 # agent_host.sendCommand("move 1")    # run!
+time.sleep(0.5)
 while world_state.is_mission_running:
     #sys.stdout.write(".")
     time.sleep(0.1)
+    count += 1
+    print("Iteration:",count)
     if world_state.number_of_observations_since_last_state > 0:
         msg = world_state.observations[-1].text
         ob = json.loads(msg)
         # print(ob)
         if "Yaw" in ob:
             current_yaw = ob[u'Yaw']
+        if "CommandsSinceLastObservation" in ob:
+            if parseCommands(ob["CommandsSinceLastObservation"]):
+                if posCounter == 0 and "XPos" in ob and "YPos" in ob and "ZPos" in ob:
+                    trackX = ob["XPos"]
+                    trackY = ob["YPos"]
+                    trackZ = ob["ZPos"]
+                posCounter += 1
+                if posCounter > HOW_LONG_TO_WAIT and "XPos" in ob and "YPos" in ob and "ZPos" in ob:
+                    # check if the agent is within a range and isn't getting anywhere despite having an action
+                    if abs(trackX - ob["XPos"]) < 1 and abs(trackY - ob["YPos"]) < 1 and abs(trackZ - ob["ZPos"]) < 1:
+                        # print("JUMPING")
+                        agent_host.sendCommand("jumpmove 1")
+                    trackX = ob["XPos"]
+                    trackY = ob["YPos"]
+                    trackZ = ob["ZPos"]
+                    posCounter == 0
         if "entities" in ob:
             # ag = {k:v for k,v in ob.iteritems() if k in ['XPos','YPos','ZPos','Yaw','Pitch','Name']}
             # agent = AgentInfo(**ag)
@@ -250,11 +351,17 @@ while world_state.is_mission_running:
             # print(agent)
             # print('Entitires condition: ')
             # print(entities)
+            # for ent in entities:
+            #     if ent[5] == "Rover":
+            #         entX = ent[0]
+            #         entY = ent[1]
+            #         entZ = ent[2]
 
+            drawMobs(entities)
             best_yaw = getBestAngle(entities, current_yaw)
 
-            print('Best yaw: '),
-            print(best_yaw)
+            # print('Best yaw: '),
+            # print(best_yaw)
             difference = best_yaw - current_yaw;
             while difference < -180:
                 difference += 360;
@@ -268,21 +375,11 @@ while world_state.is_mission_running:
             else:
                 agent_host.sendCommand("move 1")
 
-            print('Distance to goal: '),
-            print(distance_to_goal)
+            # print('Distance to goal: '),
+            # print(distance_to_goal)
 
             agent_host.sendCommand("turn " + str(difference))
-            # total_commands += 1
-            # difference = best_yaw - current_yaw;
-            # while difference < -180:
-            #     difference += 360;
-            # while difference > 180:
-            #     difference -= 360;
-            # difference /= 180.0;
-            # agent_host.sendCommand("turn " + str(difference))
-    # print
-    # text = raw_input("Enter command: ")
-    # simple_parser.parseText(text, agent_host)
+
 
     world_state = agent_host.getWorldState()
     for error in world_state.errors:
